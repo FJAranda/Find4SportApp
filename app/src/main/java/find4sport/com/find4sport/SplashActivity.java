30e42bf81b002250e62c5b0f3cd971975c26478f
Javier Aranda <javierarandacaro@gmail.com>
2017-11-15 19:21:11 +0100

    Initial commit

---
+++ app/src/main/java/find4sport/com/find4sport/SplashActivity.java
@@ -0,0 +1,31 @@
+package find4sport.com.find4sport;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final long DELAY = 2000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent().setClass(
+                        SplashActivity.this, LoginActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        };
+
+        Timer timer = new Timer();
+        timer.schedule(task, DELAY);
+    }
+}
